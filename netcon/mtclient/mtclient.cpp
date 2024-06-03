9b0216b1999dfe72f0933361dbfa094c0ae3fb98
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2024-06-03 17:03:17 +0300

    Fix compilation on Windows

--- netcon/mtclient/mtclient.cpp
+++ netcon/mtclient/mtclient.cpp
@@ -421,7 +421,7 @@
 #include "mt_net.h"
 #include "mtgametrack.h"
 #include <module/module.h>
-#include "inetgetfile.h"
+#include <netcon/inetfile/inetgetfile.h>
 #include "chat_api.h"
 #include "mtstrings.h"
 
@@ -907,7 +907,8 @@ int LoginMasterTracker(void) {
       break;
     case 4:
 #ifdef WIN32
-      ShellExecute(NULL, "open", MTADDNEWURL, NULL, NULL, SW_SHOW);
+      // TODO: reacivate for site opening
+      // ShellExecute(NULL, "open", MTADDNEWURL, NULL, NULL, SW_SHOW);
 #endif
       break;
     case UID_OK: {
@@ -2334,7 +2335,7 @@ int MTVersionCheck(void) {
 
   // Specify the correct path
   DLLddio_MakePath(fulldllpath, DLLLocalD3Dir, "mtav.dll", NULL);
-  if (!DLLmod_LoadModule(&MTAVDLLHandle, fulldllpath)) {
+  if (!DLLmod_LoadModule(&MTAVDLLHandle, fulldllpath, MODF_LAZY)) {
     DLLmprintf(0, "Unable to load Mastertracker Auto version update DLL (mtav.dll)\n");
     // Try restoring a backup of the DLL
     DLLddio_MakePath(szolddll, DLLLocalD3Dir, "mtav.dll", NULL);
