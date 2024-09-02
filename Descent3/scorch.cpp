DescentNetwork/Descent3

commit 20ed30eef804a5a251920cd8484043ef7ea48e78

Resolve out-of-bounds accesses in scorch.cpp
Giving off a shot in level 1 triggers an ASAN OOB notice without stacktrace/abort.

Descent3/scorch.cpp:200:53: runtime error: index -1 out of bounds for type 'scorch [500]'
  for (i = Scorch_start, sp = &Scorches[Scorch_start];;) {
                                                    ^

At the beginning of a level, Scorch_start is still -1. Computing
``&Scorches[Scorch_start]``, i.e. ``&Scorches[-1]``, is legal but
conceptually wrong. ``sp`` is then dereferenced inside the loop body
ahead of checking for the terminating condition.

Add a guarding termination condition in the loop head so that we
never even start the loop if Scorch_start<0. Never compute ``sp`` if
the loop is not entered.

Descent3/scorch.cpp
@@ -146,13 +146,12 @@ void ResetScorches() { Scorch_start = Scorch_end = -1; }
 // Delete the specified scorch mark
 void DeleteScorch(int index) {
   int roomface = Scorches[index].roomface;
-  scorch *sp;
-  int i;
 
   // mprintf(0,"Deleting scorch %d\n",index);
 
   // Look through all the scorches to see if there are other scorches on the same face
-  for (i = Scorch_start, sp = &Scorches[Scorch_start];;) {
+  for (int i = Scorch_start; i >= 0; ) {
+    auto sp = &Scorches[Scorch_start];
 
     if ((sp->roomface == roomface) && (i != index)) // Found another scorch
       return;
@@ -195,9 +194,8 @@ void AddScorch(int roomnum, int facenum, vector *pos, int texture_handle, float 
 
   // Count the number of scorches on this face, and bail if already at max
   int count = 0;
-  int i;
-  scorch *sp;
-  for (i = Scorch_start, sp = &Scorches[Scorch_start];;) {
+  for (int i = Scorch_start; i >= 0; ) {
+    auto sp = &Scorches[Scorch_start];
     float size = (float)sp->size / 16.0;
 
     // Increment count, and stop drawing if hit limit
@@ -252,7 +250,7 @@ void AddScorch(int roomnum, int facenum, vector *pos, int texture_handle, float 
     Scorch_start = 0;
 
   // Get a pointer to our struct
-  sp = &Scorches[Scorch_end];
+  auto sp = &Scorches[Scorch_end];
 
   // Fill in the data
   sp->roomface = roomface;
@@ -326,7 +324,8 @@ void DrawScorches(int roomnum, int facenum) {
   ASSERT(Scorch_end != -1);
 
   // Loop through all the scorches, and draw the ones for this face
-  for (i = Scorch_start, sp = &Scorches[Scorch_start];;) {
+  for (int i = Scorch_start; i >= 0; ) {
+    auto sp = &Scorches[Scorch_start];
 
     if (sp->roomface == roomface) { // Found one!
       vector right, up;

commit 64d6b6dab5c31f5b8caf9a8d01b195011d056098

Repair non-sensical scorch iterations
Gazing at my own code/modification, I find it is embarrasingly wrong.
Since ``sp`` is re-initialized at every loop iteration, setting it to
(the locally-constant) ``Scorch_start`` is incorrect. Make ``sp``
really mirror ``i`` at all times; this way, it also will not be
necessary anymore to update ``sp`` within the loop.

Fixes: 20ed30eef804a5a251920cd8484043ef7ea48e78

https://github.com/DescentDevelopers/Descent3/pull/566

Descent3/scorch.cpp
@@ -151,7 +151,7 @@ void DeleteScorch(int index) {
 
   // Look through all the scorches to see if there are other scorches on the same face
   for (int i = Scorch_start; i >= 0; ) {
-    auto sp = &Scorches[Scorch_start];
+    const auto sp = &Scorches[i];
 
     if ((sp->roomface == roomface) && (i != index)) // Found another scorch
       return;
@@ -161,11 +161,8 @@ void DeleteScorch(int index) {
       break;
 
     // Increment & wrap
-    if (++i == MAX_SCORCHES) {
+    if (++i == MAX_SCORCHES)
       i = 0;
-      sp = Scorches;
-    } else
-      sp++;
   }
 
   // If we're here, there are no other scorches on the face, so clear the flag
@@ -195,7 +192,7 @@ void AddScorch(int roomnum, int facenum, vector *pos, int texture_handle, float 
   // Count the number of scorches on this face, and bail if already at max
   int count = 0;
   for (int i = Scorch_start; i >= 0; ) {
-    auto sp = &Scorches[Scorch_start];
+    const auto sp = &Scorches[i];
     float size = (float)sp->size / 16.0;
 
     // Increment count, and stop drawing if hit limit
@@ -210,11 +207,8 @@ void AddScorch(int roomnum, int facenum, vector *pos, int texture_handle, float 
       break;
 
     // Increment & wrap
-    if (++i == MAX_SCORCHES) {
+    if (++i == MAX_SCORCHES)
       i = 0;
-      sp = Scorches;
-    } else
-      sp++;
   }
 
   // Check for scorch going off the edge of the face, and bail if does
@@ -250,7 +244,7 @@ void AddScorch(int roomnum, int facenum, vector *pos, int texture_handle, float 
     Scorch_start = 0;
 
   // Get a pointer to our struct
-  auto sp = &Scorches[Scorch_end];
+  const auto sp = &Scorches[Scorch_end];
 
   // Fill in the data
   sp->roomface = roomface;
@@ -325,7 +319,7 @@ void DrawScorches(int roomnum, int facenum) {
 
   // Loop through all the scorches, and draw the ones for this face
   for (int i = Scorch_start; i >= 0; ) {
-    auto sp = &Scorches[Scorch_start];
+    const auto sp = &Scorches[i];
 
     if (sp->roomface == roomface) { // Found one!
       vector right, up;
@@ -379,11 +373,8 @@ void DrawScorches(int roomnum, int facenum) {
       break;
 
     // Increment & wrap
-    if (++i == MAX_SCORCHES) {
+    if (++i == MAX_SCORCHES)
       i = 0;
-      sp = Scorches;
-    } else
-      sp++;
   }
 
   // Reset rendering states