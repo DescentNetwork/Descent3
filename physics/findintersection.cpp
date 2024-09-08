DescentNetwork/Descent3

commit cb5c2913b29a331231ba4268d29a15028165dccc

Resolve out-of-bounds access in fvi_room
Possibly fixes #530.

physics/findintersection.cpp:4711:64: runtime error: index -1 out of bounds for type 'fvi_face_room_list [200]'
=================================================================
SIGNAL 11 caught, aborting

physics/findintersection.cpp
@@ -4708,6 +4708,7 @@ int fvi_room(int room_index, int from_portal, int room_obj) {
           // If we hit the face...
           if (face_hit_type) {
             if ((fvi_query_ptr->flags & FQ_RECORD) && (face_info & FPF_RECORD) &&
+                Fvi_num_recorded_faces > 0 &&
                 !(Fvi_recorded_faces[Fvi_num_recorded_faces - 1].face_index == i &&
                   Fvi_recorded_faces[Fvi_num_recorded_faces - 1].room_index == room_index)) {
               ASSERT(Fvi_num_recorded_faces < MAX_RECORDED_FACES);

commit b4e8734f52241fe4d00157b61c41bd609c676201

Rework fvi_room patch
In Retribution level 15, the cinematic animation would not start
playing when entering Dravis's room (after defating Hellion).

Fixes: cb5c2913b29a331231ba4268d29a15028165dccc

https://github.com/DescentDevelopers/Descent3/pull/575

physics/findintersection.cpp
@@ -4708,8 +4708,8 @@ int fvi_room(int room_index, int from_portal, int room_obj) {
           // If we hit the face...
           if (face_hit_type) {
             if ((fvi_query_ptr->flags & FQ_RECORD) && (face_info & FPF_RECORD) &&
-                Fvi_num_recorded_faces > 0 &&
-                !(Fvi_recorded_faces[Fvi_num_recorded_faces - 1].face_index == i &&
+                !(Fvi_num_recorded_faces > 0 &&
+                  Fvi_recorded_faces[Fvi_num_recorded_faces - 1].face_index == i &&
                   Fvi_recorded_faces[Fvi_num_recorded_faces - 1].room_index == room_index)) {
               ASSERT(Fvi_num_recorded_faces < MAX_RECORDED_FACES);
               if (Fvi_num_recorded_faces < MAX_RECORDED_FACES) {