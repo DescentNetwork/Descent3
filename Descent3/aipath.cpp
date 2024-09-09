DescentNetwork/Descent3

commit bb1d6f6f857cf37322cb89fd1470776aa3d8f317

Resolve out-of-bounds access at start of level 10
Descent3/aipath.cpp:663:40: runtime error: index -1 out of bounds for type
'short unsigned int [5]'

Descent3/aipath.cpp
@@ -651,6 +651,8 @@ static inline bool AIPathAddDPathNode(ai_path_info *aip, int *slot, int *cur_nod
     status = AIPathAddDPath(aip, handle);
     if (!status)
       Int3(); // chrishack -- out of dynamic paths
+    if (aip->num_paths < 1)
+      return false;
 
     AIDynamicPath[*slot].num_nodes = MAX_NODES;
     *cur_node = 0;
@@ -660,7 +662,8 @@ static inline bool AIPathAddDPathNode(ai_path_info *aip, int *slot, int *cur_nod
   AIDynamicPath[*slot].pos[*cur_node] = *pos;
   AIDynamicPath[*slot].roomnum[(*cur_node)++] = room;
 
-  aip->path_end_node[aip->num_paths - 1] = *cur_node - 1;
+  if (aip->num_paths >= 0)
+    aip->path_end_node[aip->num_paths - 1] = *cur_node - 1;
 
   return true;
 }

commit 94a3b828ea891ee2fae638bbaad3f083384707d2

Resolve out-of-bounds access during level 1
Descent3/aipath.cpp:663:40: runtime error: index -1 out of bounds for type
'short unsigned int [5]'

Fixes: bb1d6f6f857cf37322cb89fd1470776aa3d8f317

https://github.com/DescentDevelopers/Descent3/pull/582

Descent3/aipath.cpp
@@ -626,7 +626,7 @@ static inline bool AIPathAddDPathNode(ai_path_info *aip, int *slot, int *cur_nod
   AIDynamicPath[*slot].pos[*cur_node] = *pos;
   AIDynamicPath[*slot].roomnum[(*cur_node)++] = room;
 
-  if (aip->num_paths >= 0)
+  if (aip->num_paths > 0)
     aip->path_end_node[aip->num_paths - 1] = *cur_node - 1;
 
   return true;