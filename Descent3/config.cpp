DescentNetwork/Descent3

commit 51fb3fea609e81d3897c33d901dce0b7be68c082

Resolution configuration: display current resolution in menu

Descent3/config.cpp
@@ -657,6 +657,7 @@ static void config_gamma() {
   }
 }
 
+
 //////////////////////////////////////////////////////////////////
 // VIDEO MENU
 //
@@ -666,6 +667,7 @@ struct video_menu {
   bool *filtering; // settings
   bool *mipmapping;
   bool *vsync;
+  char *resolution_string;
 
   int *bitdepth; // bitdepths
 
@@ -675,7 +677,11 @@ struct video_menu {
 
     // video resolution
     sheet->NewGroup(TXT_RESOLUTION, 0, 0);
-    sheet->AddLongButton("Change resolution", IDV_CHANGEWINDOW);
+    constexpr int RES_BUFFER_SIZE = 15;
+    resolution_string = sheet->AddChangeableText(RES_BUFFER_SIZE);
+    std::string res = Video_res_list[Game_video_resolution].getName();
+    snprintf(resolution_string, res.size() + 1, res.c_str());
+    sheet->AddLongButton("Change", IDV_CHANGE_RES_WINDOW);
 
 #if !defined(POSIX)
     // renderer bit depth
@@ -778,6 +784,9 @@ struct video_menu {
           temp_w = Video_res_list[Game_video_resolution].width;
           temp_h = Video_res_list[Game_video_resolution].height;
           Current_pilot.set_hud_data(NULL, NULL, NULL, &temp_w, &temp_h);
+
+          std::string res = Video_res_list[Game_video_resolution].getName();
+          snprintf(resolution_string, res.size() + 1, res.c_str());
         }
       }
 

commit 244513e9fecc2da1ae4e5400f42d1d4072a42685

options: resolve potential buffer overflow with resolution config button
Only a 15-byte buffer was allocated, but if the resolution string
pathalogically had a length of >15, snprintf would exceed the buffer.

Fixes: 51fb3fea609e81d3897c33d901dce0b7be68c082

https://github.com/DescentDevelopers/Descent3/pull/713

Descent3/config.cpp
@@ -782,10 +782,10 @@ struct video_menu {
 
     // video resolution
     sheet->NewGroup(TXT_RESOLUTION, 0, 0);
-    constexpr int RES_BUFFER_SIZE = 15;
-    resolution_string = sheet->AddChangeableText(RES_BUFFER_SIZE);
     std::string res = Video_res_list[Current_video_resolution_id].getName();
-    snprintf(resolution_string, res.size() + 1, res.c_str());
+    auto alloc_size = std::max(res.size() + 1, static_cast<size_t>(15));
+    resolution_string = sheet->AddChangeableText(alloc_size);
+    snprintf(resolution_string, alloc_size, res.c_str());
     sheet->AddLongButton("Change", IDV_CHANGE_RES_WINDOW);
 
     fullscreen = sheet->AddLongCheckBox("Fullscreen", Game_fullscreen);