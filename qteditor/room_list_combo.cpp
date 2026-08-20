738e8dbc0d211f966920bf07bfe3e60ba205d23c
Gravis <noreply+codeberg@adaptivetime.com>
2026-08-19 22:28:39 -0400

    Qt editor: port ObjectListCombo, RoomListCombo, TriggerListCombo
    Port three combo-box widget classes from Win32 editor as free-function
    Qt equivalents (same pattern as sound_combo):
    - populateObjectCombo: lists named objects filtered by type
    - populateRoomCombo: lists named rooms
    - populateTriggerCombo: lists triggers
    Each includes set/get selected helpers and auto-refresh on focus.

---
+++ qteditor/room_list_combo.cpp
@@ -0,0 +1,58 @@
+/*
+ * Descent 3
+ * Copyright (C) 2024 Descent Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "room_list_combo.h"
+
+#include <QComboBox>
+
+#include "room.h"
+#include "room_external.h"
+
+void populateRoomCombo(QComboBox *combo, int selected) {
+  if (combo == nullptr)
+    return;
+
+  combo->clear();
+  combo->addItem("<none>", -1);
+
+  for (int i = 0; i <= Highest_room_index; i++) {
+    if (Rooms[i].used && Rooms[i].name) {
+      int index = combo->count();
+      combo->addItem(Rooms[i].name, i);
+      if (selected == i)
+        combo->setCurrentIndex(index);
+    }
+  }
+}
+
+void setRoomComboSelected(QComboBox *combo, int selected) {
+  if (combo == nullptr)
+    return;
+  for (int i = 0; i < combo->count(); i++) {
+    if (combo->itemData(i).toInt() == selected) {
+      combo->setCurrentIndex(i);
+      return;
+    }
+  }
+}
+
+int roomComboSelected(QComboBox *combo) {
+  if (combo == nullptr)
+    return -1;
+  return combo->currentData().toInt();
+}
